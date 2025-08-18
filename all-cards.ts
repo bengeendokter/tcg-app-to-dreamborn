de24d5130ff7ba7572c1bb0514a647106eff521b
bengeendokter <benifit.arts@gmail.com>
2025-08-18 20:12:47 +0200

    Import allCards.json

---
+++ all-cards.ts
@@ -0,0 +1,8 @@
+interface AllCards {
+
+}
+
+function isAllCards(object: any): object is AllCards {
+    // TODO implement type checking logic for AllCards
+    return true;
+}
--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+import * as fs from 'fs';
+
 console.log("main.ts is running");
 
-// try to fetch the latest version of allCards.json
+// TODO try to fetch the latest version of allCards.json
 
 // if the fetch fails, use the local version
+const ALL_CARDS_PATH = "allCards.json" as const;
+const importedAllCardsJson: string = fs.readFileSync(ALL_CARDS_PATH, { encoding: 'utf-8' });
+
+// parse the JSON string into an object
+const parsedAllCardsJson: unknown = JSON.parse(importedAllCardsJson);
+
+console.log("importedAllCardsJson", importedAllCardsJson);
 
 // check if allCards.json is of correct type
 
