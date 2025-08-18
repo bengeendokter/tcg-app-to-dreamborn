af1a4ea95cb9992c709d0ae41ef2903433f0f66f
bengeendokter <benifit.arts@gmail.com>
2025-08-18 19:56:16 +0200

    Create main.ts file and add plan structure

---
+++ main.ts
@@ -0,0 +1,13 @@
+// try to fetch the latest version of allCards.json
+
+// if the fetch fails, use the local version
+
+// check if allCards.json is of correct type
+
+// import userdata.json
+
+// check if userdata.json is of correct type
+
+// create collection.csv file
+
+// create all deck.txt files
