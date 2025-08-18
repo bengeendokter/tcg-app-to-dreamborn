9ac9f72ce7ae43007c8a4dcdb286cc5892050de1
bengeendokter <benifit.arts@gmail.com>
2025-08-18 20:47:57 +0200

    Parse userdata.json

--- main.ts
+++ main.ts
@@ -8,34 +8,59 @@ const ALL_CARDS_PATH = "allCards.json" as const;
 let importedAllCardsJson: string;
 
 try {
-    importedAllCardsJson= fs.readFileSync(ALL_CARDS_PATH, { encoding: 'utf-8' });
+    importedAllCardsJson = fs.readFileSync(ALL_CARDS_PATH, { encoding: 'utf-8' });
 }
 catch (error) {
-    throw Error("Failed to read allCards.json file");
+    throw Error(`Failed to read ${ALL_CARDS_PATH}`);
 }
 
-// parse the JSON string into an object
+// parse the allCards.json string into an object
 let parsedAllCardsJson: unknown;
 
 try {
     parsedAllCardsJson = JSON.parse(importedAllCardsJson);
 }
 catch (error) {
-    throw Error("Format of allCards.json is not valid JSON");
+    throw Error(`Format of ${ALL_CARDS_PATH} is not valid JSON`);
 }
 
 if (typeof parsedAllCardsJson !== 'object' || parsedAllCardsJson === null) {
-    throw Error("Parsed allCards.json is not an object");
+    throw Error(`Parsed ${ALL_CARDS_PATH} is not an object`);
 }
 
 // check if allCards.json is of correct type
-if(!isAllCards(parsedAllCardsJson)) {
-    throw Error("Parsed allCards.json is not of type AllCards");
+if (!isAllCards(parsedAllCardsJson)) {
+    throw Error(`Parsed ${ALL_CARDS_PATH} is not of type AllCards`);
 }
 
 console.log("parsedAllCardsJson", parsedAllCardsJson);
 
 // import userdata.json
+const USER_DATA_PATH = "userdata.json" as const;
+let importedUserDataJson: string;
+
+try {
+    importedUserDataJson = fs.readFileSync(USER_DATA_PATH, { encoding: 'utf-8' });
+}
+catch (error) {
+    throw Error(`Failed to read ${USER_DATA_PATH}`);
+}
+
+// parse the allCards.json string into an object
+let parsedUserDataJson: unknown;
+
+try {
+    parsedUserDataJson = JSON.parse(importedUserDataJson);
+}
+catch (error) {
+    throw Error(`Format of ${USER_DATA_PATH} is not valid JSON`);
+}
+
+if (typeof parsedUserDataJson !== 'object' || parsedUserDataJson === null) {
+    throw Error(`Parsed ${USER_DATA_PATH} is not an object`);
+}
+
+console.log("parsedUserDataJson", parsedUserDataJson);
 
 // check if userdata.json is of correct type
 
