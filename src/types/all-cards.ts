ae068640a7a137d79a20aa18232f97119a2a30c7
bengeendokter <benifit.arts@gmail.com>
2025-08-23 12:27:35 +0200

    Implement isAllCards

--- src/types/all-cards.ts
+++ src/types/all-cards.ts
@@ -10,7 +10,43 @@ export interface AllCards {
     cards: Card[];
 }
 
-export function isAllCards(object: object): object is AllCards {
-    // TODO implement type checking logic for AllCards
+function isCard(object: object): object is Card {
+    if (!('id' in object) || typeof object.id !== 'number') {
+        return false;
+    }
+
+    if (!('number' in object) || typeof object.number !== 'number') {
+        return false;
+    }
+
+    if (!('setCode' in object) || typeof object.setCode !== 'string') {
+        return false;
+    }
+
+    if (!('code' in object) || typeof object.code !== 'string') {
+        return false;
+    }
+
+    if (!('fullName' in object) || typeof object.fullName !== 'string') {
+        return false;
+    }
+
     return true;
+}
+
+export function isAllCards(object: object): object is AllCards {
+    if (!('cards' in object)) {
+        return false;
+    }
+
+    const cards: unknown = object.cards;
+    if(!Array.isArray(cards)) {
+        return false;
+    }
+
+    if (!cards.every((card): card is object => typeof card === 'object' && card !== null)) {
+        return false;
+    }
+
+    return cards.every(card => isCard(card));
 }
