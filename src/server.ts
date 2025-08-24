9191764030a119f080f0bf32b2985d3d391e5179
bengeendokter <benifit.arts@gmail.com>
2025-08-24 16:51:03 +0200

    Implement api/deck

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { backupDeck } from './backup-deck';
+import type { DreambornDeck } from './model/dreamborn';
 
 const app = express();
 const port = 3000;
@@ -12,9 +14,21 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.post('/api/deck', (request, response) => {
-    const backupUrl: string = request.body.backupUrl;
-    response.send({ deck: 'Sample Deck Data' });
+app.post('/api/deck', async (request, response) => {
+    try {
+        const backupUrl: string = request.body.backupUrl;
+        const dreambornDeck: DreambornDeck = await backupDeck(backupUrl);
+
+        const deck: string = dreambornDeck.cards.map(card => [card.count, card.fullName].join(' ')).join('\n');
+        response.send({ deck });
+    }
+    catch (error: unknown) {
+        if (error instanceof Error) {
+            response.status(500).send({ error: error.message });
+        } else {
+            response.status(500).send({ error: 'An unknown error occurred' });
+        }
+    }
 });
 
 app.listen(port, () => {
